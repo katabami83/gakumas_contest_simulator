474ffb4b24b20c2c7affaf395afd7af8d3445804
katabami83 <katabami0141@gmail.com>
2024-11-22 17:49:36 +0900

    [update] 13期コンテスト

--- simulator/game/data/contest.js
+++ simulator/game/data/contest.js
@@ -599,4 +599,56 @@ export const data = [
       },
     ],
   },
+  {
+    id: 241124,
+    name: '第13期コンテスト',
+    period: '2024/11/24 - 12/09',
+    criteria: {
+      vocal: 35,
+      dance: 35,
+      visual: 30,
+    },
+    rank: ['vocal', 'dance', 'visual'],
+    stages: [
+      {
+        name: 'ステージ1',
+        turn: 6,
+        stageEffects: [],
+        stagePItemIds: [4241121],
+        plan: 'free',
+        turnTypes: {
+          vocal: 2,
+          dance: 2,
+          visual: 2,
+        },
+        firstTurnFirstTypeProb: 0.95,
+      },
+      {
+        name: 'ステージ2',
+        turn: 12,
+        stageEffects: [],
+        stagePItemIds: [4241122],
+        plan: 'sense',
+        turnTypes: {
+          vocal: 5,
+          dance: 4,
+          visual: 3,
+        },
+        firstTurnFirstTypeProb: 0.85,
+      },
+      {
+        name: 'ステージ3',
+        turn: 12,
+        stageEffects: [],
+        stagePItemIds: [4241123],
+        plan: 'logic',
+        turnTypes: {
+          vocal: 5,
+          dance: 4,
+          visual: 3,
+        },
+        firstTurnFirstTypeProb: 0.85,
+      },
+    ],
+  },
 ];
